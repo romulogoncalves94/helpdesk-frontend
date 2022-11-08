e59ac1dc26ce153ee65b2baa3a07145157ee372a
romulogoncalves94 <romulogoncalves94@hotmail.com>
2022-11-08 18:35:57 -0300

    feat: criado Logout

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { timeout } from 'rxjs';
+import { AuthService } from 'src/app/services/auth.service';
+
 
 @Component({
   selector: 'app-nav',
@@ -8,10 +12,16 @@ import { Router } from '@angular/router';
 })
 export class NavComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private AuthService: AuthService, private toast: ToastrService) { }
 
   ngOnInit(): void {
-    this.router.navigate(['tecnicos'])
+    this.router.navigate(['home'])
+  }
+
+  logout() {
+    this.router.navigate(['login'])
+    this.AuthService.logout()
+    this.toast.info('Logout realizado com sucesso', 'Logout', { timeOut: 7000 })
   }
 
 }
